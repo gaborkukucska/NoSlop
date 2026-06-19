e1a1b89abd3ed8945e4101f093d830a9ad99a762
gaborkukucska <gabor.kukucska@gmail.com>
2026-06-19 18:54:35 +0800

    bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 24
         targetSdk = 35
         versionCode = 1
-        versionName = "0.1.4-alpha"
+        versionName = "0.1.5-alpha"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         ndk {
