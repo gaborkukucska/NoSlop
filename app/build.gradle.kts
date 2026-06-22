4e6b24b07ca9c601410a859eba888255092adc52
Gabor Kukucska <gabor.kukucska@gmail.com>
2026-06-23 02:32:11 +0800

    0.2.0-alpha

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 24
         targetSdk = 35
         versionCode = 1
-        versionName = "0.1.5-alpha"
+        versionName = "0.2.0-alpha"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         ndk {
