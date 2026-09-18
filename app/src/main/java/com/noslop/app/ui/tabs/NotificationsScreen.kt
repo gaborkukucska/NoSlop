0b7cac1fe6edb7a39c5a18cc8e0fa144b8d3de91
Gabor Kukucska <gabor.kukucska@gmail.com>
2026-09-18 12:28:52 +0800

    fix(notifications): add missing BorderStroke import in NotificationsScreen

--- app/src/main/java/com/noslop/app/ui/tabs/NotificationsScreen.kt
+++ app/src/main/java/com/noslop/app/ui/tabs/NotificationsScreen.kt
@@ -2,6 +2,7 @@ package com.noslop.app.ui.tabs
 
 import com.noslop.app.util.tr
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
