6156b0075907a691a364095193202b43fdd4285d
Gabor Kukucska <gabor.kukucska@gmail.com>
2026-09-19 13:33:28 +0800

    version bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.noslop.app"
         minSdk = 24
         targetSdk = 35
-        versionCode = 58
-        versionName = "0.5.8-alpha"
+        versionCode = 59
+        versionName = "0.5.9-alpha"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         ndk {
